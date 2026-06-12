76810d730e7e4e75bb77e943ecd1d6679c2e8a92
thetrueryan <georgytoper@gmail.com>
2026-06-12 18:37:25 +0400

    add track data extraction and player initialization

--- src/extractor.rs
+++ src/extractor.rs
@@ -1,25 +1,93 @@
 use std::error::Error;
 
+use librespot::metadata::album;
+use tokio::signal;
+
+use librespot::core::SpotifyUri;
+use librespot::metadata::Metadata;
 use librespot::oauth::OAuthClientBuilder;
 use librespot::oauth::OAuthClient;
 use librespot::core::{SessionConfig, authentication::Credentials, session::Session};
-use crate::config::SCOPES;
+use librespot::oauth::OAuthToken;
+use librespot::metadata::{Playlist, Track};
+use librespot::playback::config::AudioFormat;
+use librespot::playback::config::PlayerConfig;
+use librespot::playback::player::{Player, PlayerEvent};
+use librespot::playback::audio_backend;
+use librespot::playback::mixer::NoOpVolume;
 
+use crate::config::SCOPES;
 use crate::config::Config;
 
-pub async fn extract_playlist(config: Config, playlist_id: String) -> Result<(), Box<dyn Error>> {
-    println!("start extracting playlist...");
-    println!("{}, {}", config.client_id, config.redirect_uri);
+
+async fn get_access_token(config: Config) -> OAuthToken {
     let scopes: Vec<&str> = SCOPES.to_vec();
     let builder: OAuthClientBuilder = OAuthClientBuilder::new(config.client_id.trim(), config.redirect_uri.trim(), scopes);        
     let client: OAuthClient = builder.build().unwrap();
-    let token = client.get_access_token().unwrap();
+    return client.get_access_token().unwrap()
+}
+
+
+pub async fn extract_track(track_id: &SpotifyUri, session: &Session) -> Result<(), Box<dyn Error>> {
+    let track: Track = Track::get(session, track_id).await.unwrap();
+
+    let track_name = track.name;
+    let album_name = track.album.name;
+    let track_number = track.number;
+    let artists_names: Vec<String> = track.artists.iter().map(|artist| artist.name.clone()).collect();
+    let album_artists_names: Vec<String> = track.album.artists.iter().map(|artist| artist.name.clone()).collect();
+    let date = track.album.date;
+    let image_url = track.album.covers.first().map(|image| format!("https://i.scdn.co/image/{}", image.id));
+    
+    let sink_builder = audio_backend::find(None).expect("Audio backends to listen not found");
+    let audio_format: AudioFormat = AudioFormat::default();
+
+    let player = Player::new(
+            PlayerConfig::default(),
+            session.clone(),
+            Box::new(NoOpVolume),               
+            move || sink_builder(None, audio_format),
+    );
+
+    player.load(track.id, true, 0);
+    let mut event = player.get_player_event_channel();
+
+    println!("Track: {}", track_name);
+    println!("Track #: {}", track_number);
+    for artist_name in artists_names.clone() { 
+        println!("Artists: {}", artist_name)
+    }
+    println!("Album: {}", album_name);
+    println!("Date: {}", date.to_string());
+    if let Some(url) = image_url {
+        println!("Image URL: {}", url);
+    } else {
+        println!("Image URL is None")
+    }
+    println!("Playing... enter Ctrl+C to exit");
+    tokio::signal::ctrl_c().await?;
+    Ok(())
+
+    
+}
+
+pub async fn extract_playlist(config: Config, playlist_uri: String) -> Result<(), Box<dyn Error>> {
+    println!("start extracting playlist...");
+    let token = get_access_token(config).await;
 
     let session: Session = Session::new(SessionConfig::default(), None);
     let credentials: Credentials = Credentials::with_access_token(token.access_token);
     session.connect(credentials, true).await?;
-
     println!("successfully connected to spotify session...");
+    
+    let playlist_id = SpotifyUri::from_uri(playlist_uri.as_str()).unwrap();
+    let playlist: Playlist = Playlist::get(&session, &playlist_id).await?;
+    
+    for i in 0..playlist.contents.items.len() {
+        let track_item = &playlist.contents.items[i];
+        let track_id = &track_item.id;
+        extract_track(track_id, &session).await?;
+    } 
 
     Ok(())
 
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 mod extractor;
 mod config;
 
-use std::env;
 use std::error::Error;
-use dotenvy::dotenv;
 
 use crate::config::Config;
 use crate::extractor::extract_playlist;
@@ -11,11 +9,11 @@ use crate::extractor::extract_playlist;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let playlist_id = "4ZlZah6Ks8E5f5GDOJns7a";
-    
+    let playlist_uri = "spotify:playlist:4ZlZah6Ks8E5f5GDOJns7a";
+
     let config: Config = Config::load_config()?;
 
-    extract_playlist(config, playlist_id.to_string()).await?;
+    extract_playlist(config, playlist_uri.to_string()).await?;
 
     Ok(())
 }
